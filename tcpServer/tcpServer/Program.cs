a2d47d621a8575fb3d79c2076046895133de7203
yoseph-abe <yoseph.abebe@lensker24.de>
2023-11-07 15:15:22 +0100

    added tcpServer

---
+++ tcpServer/tcpServer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 1984);
+            server.Start();
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] hello = new byte[1024];
+                hello = Encoding.Default.GetBytes("Hello From Server!");
+
+                stream.Write(hello, 0, hello.Length);
+
+                if (client.Connected)
+                {
+                    byte[] msg = new byte[1024];
+                    stream.Read(msg, 0, msg.Length);
+                    Console.WriteLine(Encoding.Default.GetString(msg));
+                }
+
+            }
+        }
+    }
+}
