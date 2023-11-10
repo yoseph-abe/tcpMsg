2397f1b3792ece3aac53ddefa9c6b96d6749f7ee
yoseph-abe <yoseph.abebe@lensker24.de>
2023-11-10 10:00:01 +0100

    better v

--- tcpClient/tcpClient/Program.cs
+++ tcpClient/tcpClient/Program.cs
@@ -15,42 +15,36 @@ public class Program
 
             TcpClient client = new TcpClient(serverIP, serverPort);
 
-            NetworkStream stream = client.GetStream();
-            StreamReader reader = new StreamReader(stream);
-            StreamWriter writer = new StreamWriter(stream);
-
             try
             {
-                Console.Write("msg>");
-                string input = Console.ReadLine();
-                if(input == "exit")
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.UTF8))
                 {
-                    stream.Close();
-                    client.Close();
-                    reader.Close();
-                    writer.Close();
-                    Console.WriteLine("Connection Closed!");
-                    break;
+                    Console.Write("msg>");
+                    string input = Console.ReadLine();
+                    if (input == "exit")
+                    {
+                        stream.Close();
+                        client.Close();
+                        reader.Close();
+                        writer.Close();
+                        Console.WriteLine("Connection Closed!");
+                        break;
+                    }
+
+                    //write message
+                    writer.WriteLine(input);
+                    writer.Flush();
+
+                    //read server response
+                    string serverResponse1 = reader.ReadLine();
+                    Console.WriteLine(serverResponse1);
                 }
-
-                //write message
-                writer.WriteLine(input);
-                writer.Flush();
-
-                //read server response
-                string serverResponse1 = reader.ReadLine();
-                Console.WriteLine(serverResponse1);
             }
             catch (Exception es)
             {
                 Console.WriteLine("Error: " +  es.Message);
-            }
-            finally
-            {
-                stream.Close();
-                client.Close();
-                reader.Close();
-                writer.Close();
             }
         }
     }
